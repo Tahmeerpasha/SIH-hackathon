907f56e8d71bdea48c2b8b9624e222b6277dbb7d
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-09-25 11:28:12 +0530

    final update 1.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     <div className='flex flex-row flex-wrap justify-center w-full bg-[#8674f0] h-full rounded-[23px] drop-shadow-lg'>
       <div className='flex flex-col justify-center md:items-start md:ml-[90px] drop-shadow-xl md:px-[5vw] md:py-[4vh] px-[5vw] my-3'>
         <h1 className='text-2xl'>Professor Guide:-</h1>
-        <p className='hover:text-lg cursor-pointer'>Prof - Vijayan balaji</p>
-        <p className='hover:text-lg cursor-pointer'>Prof - shravana kayapanni</p>
+        <p className='hover:text-lg cursor-pointer'>Dr.Balaji Vijayan</p>
+        <p className='hover:text-lg cursor-pointer'>Prof.Sharavana Kaniyappan</p>
       </div>
       <div className='flex flex-col justify-center items-center ml-[90px]drop-shadow-xl  md:px-[5vw] md:py-[4vh] px-[5vw] my-3'>
         <p className='text-bold pb-[10px] pt-[10px]'>This project was made for the SIH hackathon</p>
-        <Image src={footerlogo} objectFit="cover" layout="responsive" className='shadow-lg rounded-[5px] hover:shadow-2xl'></Image>
+        <Image src={footerlogo} objectFit="cover" layout="responsive" alt='footerlogo' className='shadow-lg rounded-[5px] hover:shadow-2xl'></Image>
       </div>
       <div className='flex flex-col md:px-[5vw] md:py-[4vh] px-[5vw] my-3'>
         <h3 className='text-2xl text-bold'>Note:</h3>
